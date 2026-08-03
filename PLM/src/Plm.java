7a010e113d758758244c2c17f7934b7050810d5b
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-03 13:55:42 +0200

    feat: Add displayPlanes method.

--- PLM/src/Plm.java
+++ PLM/src/Plm.java
@@ -6,35 +6,47 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * 
  */
 public class Plm {
-	enum Phase {
-		ETUDE_DE_FAISABILITE, CONCEPTION, DEFINITION, CONSTRUCTION, EN_SERVICE, CLOTURE
+	private static void displayPlanes(Hashtable<Integer, ArrayList<ArrayList<String>>> planes) {
+		System.out.println("Voici la list de tous les avions.");
+		for (Map.Entry<Integer, ArrayList<ArrayList<String>>> plane : planes.entrySet()) {
+			ArrayList<String> planeData = plane.getValue().get(0);
+			System.out.println("Id n°" + plane.getKey() + ": Avion " + planeData.get(0) + " qui est en "
+					+ planeData.get(1) + " est destiné au " + planeData.get(2));
+		}
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String[] phaseCurrently = { "étude de faisabilité", "conception", "définition", "construction", "en service",
+				"clôturé" };
 		Hashtable<Integer, ArrayList<ArrayList<String>>> planes = new Hashtable<>();
 
 		ArrayList<ArrayList<String>> plane1 = new ArrayList<ArrayList<String>>();
 		ArrayList<ArrayList<String>> plane2 = new ArrayList<ArrayList<String>>();
 		ArrayList<ArrayList<String>> plane3 = new ArrayList<ArrayList<String>>();
-		
-		plane1.add(new ArrayList<>(Arrays.asList("A320", Plm.Phase.ETUDE_DE_FAISABILITE.toString(), "fret")));
-		plane2.add(new ArrayList<>(Arrays.asList("A400M", Plm.Phase.DEFINITION.toString(), "militaire")));
-		plane3.add(new ArrayList<>(Arrays.asList("A300", Plm.Phase.CONSTRUCTION.toString(), "avions d'affaires")));
+		ArrayList<ArrayList<String>> plane4 = new ArrayList<ArrayList<String>>();
+
+		plane1.add(new ArrayList<>(Arrays.asList("A320", phaseCurrently[0], "fret")));
+		plane2.add(new ArrayList<>(Arrays.asList("A400M", phaseCurrently[2], "militaire")));
+		plane3.add(new ArrayList<>(Arrays.asList("A300", phaseCurrently[3], "avions d'affaires")));
+		plane4.add(new ArrayList<>(Arrays.asList("A380", phaseCurrently[5], "avions d'affaires")));
 
 		// Adding elements
 		planes.put(1, plane1);
 		planes.put(2, plane2);
 		planes.put(3, plane3);
+		planes.put(4, plane4);
 
-		System.out.print(planes);
+		displayPlanes(planes);
+		;
 
 	}
 
