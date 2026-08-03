80edacda5cf567d95698de63d4604a27248404b0
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-03 13:37:47 +0200

    Create a dictionary of plane and add multiple planes.

--- PLM/src/Plm.java
+++ PLM/src/Plm.java
@@ -1,17 +1,40 @@
+
 /**
  * 
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
 
 /**
  * 
  */
 public class Plm {
+	enum Phase {
+		ETUDE_DE_FAISABILITE, CONCEPTION, DEFINITION, CONSTRUCTION, EN_SERVICE, CLOTURE
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Hashtable<Integer, ArrayList<ArrayList<String>>> planes = new Hashtable<>();
+
+		ArrayList<ArrayList<String>> plane1 = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> plane2 = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> plane3 = new ArrayList<ArrayList<String>>();
+		
+		plane1.add(new ArrayList<>(Arrays.asList("A320", Plm.Phase.ETUDE_DE_FAISABILITE.toString(), "fret")));
+		plane2.add(new ArrayList<>(Arrays.asList("A400M", Plm.Phase.DEFINITION.toString(), "militaire")));
+		plane3.add(new ArrayList<>(Arrays.asList("A300", Plm.Phase.CONSTRUCTION.toString(), "avions d'affaires")));
+
+		// Adding elements
+		planes.put(1, plane1);
+		planes.put(2, plane2);
+		planes.put(3, plane3);
+
+		System.out.print(planes);
 
 	}
 
