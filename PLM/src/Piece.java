03e0f936a651b2eb8e1c35d5fe3ed97a562fafce
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-04 08:07:26 +0200

    feat: Add Piece.java file and init a 2D array.

--- PLM/src/Piece.java
+++ PLM/src/Piece.java
@@ -6,5 +6,28 @@
  * 
  */
 public class Piece {
-
+	String piecesFouga = "Fouga Magister";
+	String piecesInstruments = "Instruments";
+	String piecesDiversCategory = "Pièces diverses";
+	
+	
+	String[][] shop = {
+		{"Tuyères (x2)", "450", piecesFouga},
+		{"Vanne de Réglage", "150", piecesFouga},
+		{"Verins de train", "150", piecesFouga},
+		{"Capots moteurs", "100", piecesFouga},
+		{"Échangeur", "90", piecesFouga},
+		{"Poignée de Gaz", "150", piecesFouga},
+		{"Bonbonne air", "90", piecesFouga},
+		{"Planche de bord fouga", "1200", piecesInstruments},
+		{"Manomètre d'admission", "35", piecesInstruments},
+		{"Tachymère", "39", piecesInstruments},
+		{"Anémomètre (ASI)", "49", piecesInstruments},
+		{"6 Jambes de train", "200", piecesInstruments},
+		{"Renvoi sécat", "35", "Breguet 'Deux ponts'"},
+		{"Filtre à huile", "30", piecesDiversCategory},
+		{"Pièces dassault, etc", "30", piecesDiversCategory},
+		{"Connecteurs", "15", piecesDiversCategory},
+		{"Pièces alu", "5", piecesDiversCategory},
+	};
 }
